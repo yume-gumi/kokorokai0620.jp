a5d17db273552e93d435e195ac61b15332eb7c64
ku-chan18 <orangelaub@gmail.com>
2026-02-04 13:57:40 +0900

    ぽんてさんひととおり完成

--- page-ponte.php
+++ page-ponte.php
@@ -1,14 +1,413 @@
 <?php get_header();  ?>
 
 <!--  MAIN  -->
-<main id="ponte">
+<main id="ponte" class="service_page">
+
+  
+  <h4 class="service-head">就労継続支援B型 ぽんて</h4>
+
+  <div class="top-bg wave-pink"></div>
+  
+  <section id="top-mv">
+
+    <div class="mv-container">
+
+      <div class="mv-frame">
+        <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_mv.png" alt="" class="ぽんてmainvisual">
+
+        <div class="cc bg-pink-2nd">
+          <p>長く在宅生活を送られている方の社会生活が、<br>
+            労働やその他の活動を通していきいきしたものに<br>
+            なるようお手伝いします。</p>
+        </div>
+      </div>
+
+      <div class="cc-outer bg-pink">
+
+        <div class="cc-inner">
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_img01.png" alt="ponte_img01">
+          <div class="cc-text">
+            <p>ご自身の持っている力を発揮し、自分のペースで一歩ずつ<br>
+                新しい生活を始めたいあなたを、わたしたちが地域との<br>
+                架け橋としてサポートします。</p>
+          </div>
+        </div>
+
+        <div class="cc-inner">
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_img02.png" alt="ponte_img02">
+          <div class="cc-text">
+            <p>退院したばかりなので、まずは生活リズムを整えたい。<br>
+                今はデイケアに通っているけど、そこからステップアップする自信がない。<br>
+                一度は就職したけど挫折してしまったから、ゆっくり再チャレンジしたい…何でもご相談ください。</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!--  mv-container  -->
+  </section>
+  <!--  bg-img  -->
+
+  </section>
+  <!--  plus-top  -->
+
+
+  <section id="policy" class="bg-blue">
+
+    <div class="wave-bottom  wave-pink"></div>
+
+
+
+    <h3>事業所方針</h3>
+
+    <div class="policy-container">
+
+
+      <div class="policy-outer">
+
+        <div class="policy-brock">
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_policy01.png" alt="面談や振り返りの時間を大切に、本人の気持ちを確認しながら進めていきます。">
+        </div>
+        <div class="policy-brock">
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_policy02.png" alt="よりステップアップを目指していく方に、弾みをつけられるように支援していきます。">
+        </div>
+        <div class="policy-brock">
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_policy03.png" alt="自分の持っている強みを生かす視点で活動に取り組んでいきます。">
+        </div>
+        <div class="policy-brock">
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_policy04.png" alt="生活していく力、コミュニケーション能力を獲得していく時間を作っていきます。">
+        </div>
+        <div class="policy-brock">
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_policy05.png" alt="アットホームな雰囲気で安心して通えるように心がけています。">
+        </div>
+
+
+
+      </div>
+      <!--  policy-outer  -->
+
+    </div>
+    <!--  policy-contens  -->
+
+  </section>
+  <!--  policy  -->
+
+
+  <section id="schedule" class="bg-yellow">
+
+    <div class="wave-bottom  wave-blue"></div>
+
+    <div class="timeline-container">
+
+      <h3>1日のスケジュール</h3>
+
+      <ul class="timeline">
+
+        <li class="timeline-brock">
+
+          <div class="time">9:00~</div>
+
+          <div class="timeline-text">
+            <p>健康・身だしなみチェック、
+              作業室内外の清掃から始めます。</p>
+            <span>※送迎の方は9：00から順次お迎え</span>
+          </div>
+
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_tl01.png" alt="plus_tl01">
+
+        </li>
+
+
+        <li class="timeline-brock">
+
+          <div class="time">10:00~</div>
+
+          <div class="timeline-text">
+            <p>作業開始。途中10分の休憩を挟みながらの軽作業が中心ですが、ポスティングや施設外就労もあります。</p>
+          </div>
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_tl02.png" alt="plus_tl02">
+
+        </li>
+
+
+        <li class="timeline-brock">
+
+          <div class="time">12:00~</div>
+
+          <div class="timeline-text">
+            <p>お昼休憩を１時間取ります。ご持参いただいたお弁当、または仕出し弁当（１食¥350）でランチタイムです。</p>
+          </div>
+
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_tl03.png" alt="plus_tl03">
+
+        </li>
+
+        <li class="timeline-brock">
+
+          <div class="time">13:00~</div>
+
+          <div class="timeline-text">
+            <p>午後作業開始。ＳＳＴ（社会生活技能訓練）や買い物等の外出、運動をすることもあります。</p>
+          </div>
+
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_tl04.png" alt="plus_tl04">
+
+        </li>
+
+        <li class="timeline-brock">
+
+          <div class="time">14:00~</div>
+
+          <div class="timeline-text">
+            <p>一日の振り返りをし、面談を行います。</p>
+            <span>※送迎の方は15：00から順次送り</span>
+          </div>
+
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_tl05.png" alt="plus_tl04">
+
+        </li>
+
+        <li class="timeline-brock">
+
+          <div class="time">15:00~</div>
+
+          <div class="timeline-text">
+            <p>作業終了です。送迎の方は順次ご自宅までお送りします。</p>
+          </div>
+
+          <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/ponte_tl06.png" alt="plus_tl05">
+
+        </li>
+
+
+      </ul>
+      <!--  timeline  -->
+
+    </div>
+    <!--  timeline-container  -->
+
+  </section>
+  <!--  schedule  -->
+
+  <section id="flow" class="bg-green">
+
+    <div class="wave-bottom wave-yellow"></div>
+
+
+    <div class="flow-container">
+
+      <h3>ご利用の流れ</h3>
+
+      <div class="flow-outer flow-plus">
+
+        <div class="flow-brock">
+
+          <span class="step-icon">
+            <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/icon/step1.svg" alt="step1">
+          </span>
+
+          <div class="flow-card  step1">
+            <div class="flow-title  step1">ご相談</div>
+            <p class="step1">ご利用や見学・体験についてなど、お気軽にご相談ください。<br>
+              利用希望の方は、利用計画表や受給者証の手続きが必要になりますのであわせておたずねください。</p>
+          </div>
+
+        </div>
+        <div class="flow-brock">
+
+          <span class="step-icon">
+            <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/icon/step2.svg" alt="step2">
+          </span>
+
+          <div class="flow-card  step2">
+            <div class="flow-title  step2">体験・見学</div>
+            <p class="step2">施設を見学していただきます。<br>
+              一日の流れを実際に体験することも可能です。<br>
+              3日間まで体験利用することができます。</p>
+          </div>
+
+        </div>
+        <div class="flow-brock">
+
+          <span class="step-icon">
+            <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/icon/step3.svg" alt="step3">
+          </span>
+
+          <div class="flow-card  step3">
+            <div class="flow-title  step3">契約</div>
+            <p class="step3">契約書を交わし、利用開始日や回数等の調整を行います。<br>
+              基本的に週５日利用となります。</p>
+          </div>
+
+        </div>
+        <div class="flow-brock">
+
+          <span class="step-icon">
+            <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/icon/step4.svg" alt="step4">
+          </span>
+
+          <div class="flow-card  step4">
+            <div class="flow-title  step4">ご利用開始</div>
+            <p class="step4">いよいよ利用開始！<br>
+                自分のペースで楽しく作業しましょう。</p>
+          </div>
+
+        </div>
+
+      </div>
+      <!--  flow-outer  -->
+
+      <div class="flow-2nd-outer bg-green-4th">
+        <ul>
+          <li class="flow-li-plus">
+            <div class="flow-2nd-title">対象となる方</div>
+            <p>療育手帳か精神保健福祉手帳または
+              自立支援医療受給者証をお持ちの方が対象です。
+              <span>※身体障害者手帳の方は要相談</span>
+              <span>※かかりつけの病院等があり、服薬管理が出来るよう心がけている方。</span>
+              <span>（病院等がない方はご相談ください。）</span>
+            </p>
+          </li>
+
+          <li class="flow-li-plus">
+            <div class="flow-2nd-title">ご利用料金</div>
+            <p>所得に応じて異なります(1カ月0円～37,800円)<br>
+              詳細についてはお電話やメールでお問い合わせください。<br>
+              週１日からご利用いただけます。</p>
+          </li>
+
+          <li class="flow-li-plus">
+            <div class="flow-2nd-title">お食事について</div>
+            <p>給食はございません。お弁当をご持参いただくか、<br>
+                事業所で仕出し弁当（1食350円）をご注文ください。</p>
+          </li>
+
+          <li class="flow-li-plus">
+            <div class="flow-2nd-title">送迎について</div>
+            <p>なるべくご希望に沿うように送迎をしておりますが、都合により乗降場所、<br>
+                送迎時間はご希望に添えない場合もございます。</p>
+          </li>
+
+        </ul>
+
+      </div>
+      <!--  flow-2nd-outer  -->
+
+      <div class="flow-contact flow-contact-plus">
+
+        <h4>お気軽にご相談ください</h4>
+        <p class="disc">見学・体験に関する事、ご利用に関する事など、お気軽にご相談ください。</p>
+        <!--  <div  class="flow-contact-outer">  -->
+        <div class="flow-contact-inner">
+          <div class="tell">
+            <img src="<?php echo  get_theme_file_uri();  ?>/assets/images/icon/tellphone.svg" alt="tell-icon">
+            <p class="tell-number">049-293-6729</p>
+          </div>
+          <span>平日 9:00~17:00まで</span>
+        </div>
+
+        <a class="btn  contact" href="<?php echo  esc_url(home_url('/contact'));  ?>">
+          <img class="mail-icon" src="<?php echo  get_theme_file_uri();  ?>/assets/images/icon/mail.svg" alt="mail  icon">
+          お問い合わせ
+          <span class="btn-circle"></span>
+        </a>
+
+
+        <!--  </div>  -->
+        <!--  flow-contact-outer  -->
+
+
+      </div>
+      <!--  flow-container  -->
+    </div>
+    <!--  flow-contact  -->
+
+  </section>
+  <!--  flow  -->
+
+
+  <section id="address" class="bg-orange">
+    <div class="wave-bottom  wave-green"></div>
+
+
+    <div class="address-container">
+
+      <div class="address-outer">
+
+        <ul class="address-list">
+          <li>
+            <div>事業所名</div>
+            <p>就労継続支援B型　ぽんて</p>
+          </li>
+
+          <li>
+            <div>指定事業所番号</div>
+            <p>1113000374</p>
+          </li>
+
+          <li>
+            <div>所在地</div>
+            <p>埼玉県ふじみ野市松山 カントー商事ビル１Ｆ</p>
+          </li>
+
+          <li>
+            <div>連絡先</div>
+            <div class="address-tell">
+              <p>電話:049-290-3966　FAX:049-264-8070</p>
+            </div>
+          </li>
+
+          <li>
+            <div>定員</div>
+            <p>20名</p>
+          </li>
+
+          <li>
+            <div>職員配置</div>
+            <p>3名</p>
+          </li>
+        </ul>
+
+      </div>
+      <!--  address-outer  -->
+
+      <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12932.859472812632!2d139.5089669668786!3d35.868250949099235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018dcb288e45ea1%3A0xd155c15816bbd7fd!2z77yI5qCq77yJ44Kr44Oz44OI44O85ZWG5LqL!5e0!3m2!1sja!2sjp!4v1770174914341!5m2!1sja!2sjp"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+
+      <div class="access">
+        <p class="access-title">アクセス</p>
+
+        <div class="access-brock">
+          <div class="access-tool">
+            <div class="access-01">【電車】</div>
+            <p class="access-02">・東武東上線上福岡駅より徒歩15分</p>
+          </div>
+        </div>
+
+                </div>
+                <!--  access-brock  -->
+
+                <p  class=" access-car">お車、または自転車でお越しの際は、敷地内に
+              駐車場と駐輪場がございますのでご利用下さい。</p>
+
+            </div>
+            <!--  access  -->
+
+          </div>
+          <!--  address-container  -->
+
+
+  </section>
+  <!--  address  -->
+
 
 
 
     <?php
     $args  =  [
         "top-wave"  =>  "orange",
-        "background"  =>  "pink",
+        "background"  =>  "white",
         "service"  =>  true
     ];
     get_template_part("parts/footer_top",  null, $args);
